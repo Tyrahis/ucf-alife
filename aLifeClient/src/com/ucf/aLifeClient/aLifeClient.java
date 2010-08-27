fd7e54be817ef5491e59a318daf833e08ce9c893
Amos Kittelson <Amos.Kittelson@gmail.com>
2010-08-26 23:00:39 -0400

    Initial Commit

---
+++ aLifeClient/src/com/ucf/aLifeClient/aLifeClient.java
@@ -0,0 +1,13 @@
+package com.ucf.aLifeClient;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class aLifeClient extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
